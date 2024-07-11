bf6d865bf88a9d9796d463bd9a67598cc8cc78f3
JerryMouseZ <1711333@mail.nankai.edu.cn>
2024-07-11 12:43:03 +0800

    finish enum and structure

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -6,12 +6,10 @@
 // Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0.clone());
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -21,9 +19,9 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
     vec
 }
